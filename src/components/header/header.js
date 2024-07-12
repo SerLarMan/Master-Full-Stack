7b7d53ffd8253a4dc1e797082926caf43e93d3b3
Sergio Lara Mantas <sergio.lara@unimat.es>
2024-07-12 13:53:30 +0200

    Cambios menores

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,13 @@
 export function setUpHeader(component) {
   component.textContent = "";
 
+  const h1 = document.createElement("h1");
+  h1.textContent = "GAMES HUB";
+
+  const span = document.createElement("span");
+  span.textContent = "Tus puntos: ";
+
+  component.append(h1);
+  component.append(span);
   return component;
 }
