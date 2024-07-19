a6530d8debf32dabc25e4226a7ddcd02862a7755
Sergio Lara Mantas <sergio.lara@unimat.es>
2024-07-19 13:42:13 +0200

    Fixes

--- src/components/Experience/experience.js
+++ src/components/Experience/experience.js
@@ -3,14 +3,20 @@ import "../../styles/global.scss";
 
 export function setUpExperience() {
   const experience = document.createElement("section");
+  experience.classList.add("experience");
 
   const sectionTitle = document.createElement("div");
+  sectionTitle.classList.add("sectionTitle");
 
   const h2 = document.createElement("h2");
   h2.textContent = "EXPERIENCE";
   h2.id = "experience";
 
+  const line = document.createElement("div");
+  line.classList.add("line");
+
   sectionTitle.append(h2);
+  sectionTitle.append(line);
   experience.append(sectionTitle);
 
   return experience;
